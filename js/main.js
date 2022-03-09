b606151c1cd309501b9bf53ec44304b206b086c5
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-03-09 16:29:28 +0100

    finished - with bonus - dont blame for height card

--- js/main.js
+++ js/main.js
@@ -45,17 +45,47 @@ const team = [
 ];
 
 
-for (let x in team) {
-    document.getElementById("my-card-team").innerHTML = (`
-    <div class="col-4">
-        <div class="card">
-        <img src="img/${team[x].image}" class="card-img-top" alt="photo of Wayne Barnett">
-        <div class="card-body text-center">
-            <h4 class="card-text">${team[x].name}</h4>
-            <p class="card-text">${team[x].role}</p>
-        </div>
-    </div>`);
-    console.log(team[x]);
+//? sconponiamo il for in in una funzione per creare più card nuove
+// | occhio alle classi boostrap
+
+function createCard(mate) {
+    const card = document.createElement('div');
+    card.classList.add('col');
+
+    const cardElements = document.createElement('div');
+    cardElements.classList.add('card');
+
+    const img = document.createElement('img');
+    img.classList.add('card-img-top');
+    img.src = `img/${mate.image}`;
+    img.alt = `photo of ${mate.name}`;
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body', 'text-center');
+
+
+    const name = document.createElement('h4');
+    const role = document.createElement('p');
+
+    card.appendChild(cardElements);
+    cardElements.appendChild(img);
+    cardElements.appendChild(cardBody);
+    cardBody.appendChild(name);
+    cardBody.appendChild(role);
+
+    name.innerHTML = mate.name;
+    role.innerHTML = mate.role;
+    return card;
 }
 
+//? mi gnero le carte con un ciclo for
+const container = document.querySelector('#team-card');
 
+function generateCard() {
+    for (let i = 0; i < team.length; i++) {
+        const card = createCard(team[i]);
+        container.appendChild(card);
+    };
+}
+
+generateCard();
